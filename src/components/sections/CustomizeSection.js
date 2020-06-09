9d443020e1aa126a3289befe8a0c862350533fdf
Jarod Peachey <jwpeachey107@aol.com>
2020-06-09 15:27:48 -0400

    Updates

--- src/components/sections/CustomizeSection.js
+++ src/components/sections/CustomizeSection.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Section from '../layout/Section';
@@ -7,9 +7,31 @@ import { ThemeContext } from '../theme';
 import Spacer from '../Spacer';
 import paintBucket from '../../images/icons8-fill-color-100.png';
 import slider from '../../images/icons8-tune-100-2.png';
+import Switch from 'react-switch';
+import Card from '../Card';
+import Button from '../Button';
+import { loadStripe } from '@stripe/stripe-js';
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(
+  'pk_test_51Gr3KVKyL3kUtkPFJMQdsezF9hqGudJNNnwfdA9ZdH4i7MCdwni4qjxl32KSe1ClUpdapbLCMUkMeLfBeEHbwm5G00sPUTEKHc'
+);
 
 const CustomizeSection = ({ data }) => {
+  const yearlyPriceID = 'price_1Gr3MLKyL3kUtkPFnhncjTRQ';
+  const monthlyPriceID = 'price_1Gr3MLKyL3kUtkPFNHorrSve';
+
   const theme = useContext(ThemeContext);
+
+  const [inputChecked, setInputChecked] = useState(false);
+
+  const customOnChange = (setChecked) => {
+    setInputChecked(setChecked);
+  };
+
+  const onClick = async () => {
+    // TODO: Set FaunaDB temp selected plan option
+  };
 
   return (
     <Section
@@ -19,91 +41,130 @@ const CustomizeSection = ({ data }) => {
         display: block !important;
         padding-bottom: 150px;
         margin-bottom: -150px;
-  @media (min-width: 576px) {
-
-  }
-  @media (min-width: 670px) {
-
-  }
-  @media (min-width: 850px) {
-
-  }
-  @media (min-width: 1000px) {
-
-  }
-  @media (min-width: 1300px) {
-
-  }
-  @media (min-width: 1500px) {
-
-  }
-  @media (min-width: 1677px) {
-
-  }
       `}
       background='#ffffff'
     >
-      <h1>Fully Customizable</h1>
-      <p className='subtitle'>
-        Customize everything, from color theme, form styles, messages, and more.
+      <h1 className='center'>Dead-Simple Pricing</h1>
+      <p className='center'>
+        No hidden costs or surprise charges: one rate for everything you need
+        for comments on your site.
       </p>
-      <Spacer height={64} />
-      <Row spacing={[12, 24]} breakpoints={[769]}>
-        <div widths={[4]}>
-          <Feature>
-            <FeatureIcon offsetY={-2} offsetX={4}>
-              <img src={slider} />
-            </FeatureIcon>
-            <FeatureContent>
-              <FeatureTitle>Styling Dashboard</FeatureTitle>
-              <FeatureSubtitle>
-                Staticbox comes with an easy-to-use styles dashboard. Customize
-                the color palette, input styling, and font through an intuitive
-                UI.
-              </FeatureSubtitle>
-            </FeatureContent>
-          </Feature>
-          <Feature>
-            <FeatureIcon offsetY={-2} offsetX={4}>
-              <img src={paintBucket} />
-            </FeatureIcon>
-            <FeatureContent>
-              <FeatureTitle>Custom CSS</FeatureTitle>
-              <FeatureSubtitle>
-                Take your customization to the next level with support for
-                custom CSS. Staticbox allows you to fully customize every CSS
-                class used. And if you accidently remove the CSS, Staticbox
-                comes with a backup so your comments are still styled 👊
-              </FeatureSubtitle>
-            </FeatureContent>
-          </Feature>
-          <Feature>
-            <FeatureIcon offsetY={0} offsetX={1}>
-              <img src={paintBucket} />
-            </FeatureIcon>
-            <FeatureContent>
-              <FeatureTitle>Custom Text</FeatureTitle>
-              <FeatureSubtitle>
-                Interact with your users the way you want. Staticbox lets you
-                customize the headings, messages and input text, so you can show
-                your site's personality through your commenting system.
-              </FeatureSubtitle>
-            </FeatureContent>
-          </Feature>
-        </div>
-        <div widths={[8]}>
-          <img
-            style={{ width: '100%' }}
-            src='https://cosmic-s3.imgix.net/9ccb68d0-8c9a-11ea-ac68-c50ccd313395-cosmic-dash.png?w=2000&auto=format'
-          />
-        </div>
-      </Row>
-      {/* <Row spacing={[12, 32]} breakpoints={[650, 1100, 2900]}> */}
-
-      {/* </Row> */}
+      <ToggleContainer>
+        <span style={{ fontWeight: !inputChecked ? 'bold' : 400 }}>
+          Monthly
+        </span>
+        <Switch
+          checked={inputChecked}
+          onChange={customOnChange}
+          uncheckedIcon={false}
+          checkedIcon={false}
+          boxShadow='0px 0px 9px -3px #666'
+          activeBoxShadow='none'
+          id='material-switch'
+          offColor={theme.color.primary.main}
+          onColor={theme.color.primary.main}
+          offHandleColor='#ffffff'
+          onHandleColor='#ffffff'
+          className='mx-3 react-switch'
+        />
+        <span style={{ fontWeight: inputChecked ? 'bold' : 400 }}>Yearly</span>
+      </ToggleContainer>
+      <PricingCard
+        customStyles={`
+        max-width: 350px;
+        margin: 38px auto;
+      `}
+      >
+        <PriceIndicator></PriceIndicator>
+        <Price>$10</Price>
+        <PriceFrequency>per month</PriceFrequency>
+        <List>
+          <ListItem>
+            <FontAwesomeIcon
+              style={{ color: theme.color.primary.main, marginRight: 8 }}
+              icon='check'
+            />
+            Fully customizable form
+          </ListItem>
+          <ListItem>
+            <FontAwesomeIcon
+              style={{ color: theme.color.primary.main, marginRight: 8 }}
+              icon='check'
+            />
+            Simple integration
+          </ListItem>
+          <ListItem>
+            <FontAwesomeIcon
+              style={{ color: theme.color.primary.main, marginRight: 8 }}
+              icon='check'
+            />
+            Bulk moderation
+          </ListItem>
+          <ListItem>
+            <FontAwesomeIcon
+              style={{ color: theme.color.primary.main, marginRight: 8 }}
+              icon='check'
+            />
+            Spam filtering
+          </ListItem>
+          <ListItem>
+            <FontAwesomeIcon
+              style={{ color: theme.color.primary.main, marginRight: 8 }}
+              icon='check'
+            />
+            Custom messages and branding
+          </ListItem>
+          <ListItem>
+            <FontAwesomeIcon
+              style={{ color: theme.color.primary.main, marginRight: 8 }}
+              icon='check'
+            />
+            Free support
+          </ListItem>
+        </List>
+        <Button onClick={onClick} className='full-width mt-6 mb-4'>
+          Start Free Trial
+        </Button>
+      </PricingCard>
     </Section>
   );
 };
+
+const List = styled.div`
+  margin: 0 auto;
+  text-align: left;
+  margin-top: 24px;
+`;
+
+const ListItem = styled.div`
+  font-size: 16px;
+  margin: 4px 0;
+`;
+
+const PricingCard = styled(Card)`
+  padding: 16px;
+  box-shadow: 1px 0px 7px 0px #666;
+`;
+
+const PriceIndicator = styled.span`
+  font-size: 26px !important;
+  font-weight: bold !important;
+`;
+const Price = styled.span`
+  font-size: 64px !important;
+  font-weight: bold !important;
+`;
+const PriceFrequency = styled.div`
+  font-size: 18px !important;
+  font-weight: 400 !important;
+`;
+
+const ToggleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+`;
 
 const Feature = styled.div`
   width: 100%;
