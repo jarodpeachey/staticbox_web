39dd2f0ae2abd57b3f150b201189f6d7c3f52732
Jarod Peachey <jwpeachey107@aol.com>
2020-06-11 13:11:14 -0400

    Add FaunaDB function

--- src/functions/subscribe.js
+++ src/functions/subscribe.js
@@ -3,38 +3,48 @@ const stripe = stripeSdk(
   'pk_test_51Gr3KVKyL3kUtkPFJMQdsezF9hqGudJNNnwfdA9ZdH4i7MCdwni4qjxl32KSe1ClUpdapbLCMUkMeLfBeEHbwm5G00sPUTEKHc'
 );
 const nodemailer = require('nodemailer');
+const faunadb = require('faunadb');
+
+const q = faunadb.query;
+
+const client = new faunadb.Client({
+  secret: 'fnADq29sx9ACE4FItI0Ps8suOAzL0UHyqDNFNjgV',
+});
 
 exports.handler = async (event, context) => {
-  const sig = event.headers['stripe-signature'];
-
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'jarodpeachey@gmail.com',
-      pass: 'jpdevelop_google1',
-    },
-  });
-
-  const mailOptions = {
-    from: 'jarodpeachey@gmail.com',
-    to: 'jwpeachey107@aol.com',
-    subject: 'Staticbox Test Email',
-    text: `${JSON.stringify(event)}`,
-  };
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      test: true,
-      testTwo: 'test',
-    }),
-  };
+  client
+    .query(
+      q.Let(
+        {
+          user: q.Get(
+            q.Match(
+              q.Index('user_by_email'),
+              event.body.data.object.customer_email
+            )
+          ),
+          userRef: q.Select('ref', q.Var('user')),
+        },
+        q.Update(q.Var('userRef'), {
+          data: {
+            plan: event.body.data.object.subscription,
+          },
+        })
+      )
+    )
+    .then((res) => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          res,
+        }),
+      };
+    })
+    .catch((err) => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          err,
+        }),
+      };
+    });
 };
