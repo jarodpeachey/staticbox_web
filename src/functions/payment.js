b891166a5a9d33a4bc2b5da91f8906f5c50062f9
Jarod Peachey <jwpeachey107@aol.com>
2020-06-13 07:14:40 -0400

    Another update

--- src/functions/payment.js
+++ src/functions/payment.js
@@ -22,43 +22,42 @@ exports.handler = async (event, context) => {
     secret: 'fnADq29sx9ACE4FItI0Ps8suOAzL0UHyqDNFNjgV',
   });
 
-  // await client
-  //   .query(
-  //     q.Let(
-  //       {
-  //         user: q.Get(
-  //           q.Match(q.Index('user_by_email'), body.data.object.customer_email)
-  //         ),
-  //         userRef: q.Select('ref', q.Var('user')),
-  //       },
-  //       q.Update(q.Var('userRef'), {
-  //         data: {
-  //           plan: body.data.object.subscription,
-  //           stripeId: body.data.object.id,
-  //         },
-  //       })
-  //     )
-  //   )
-  //   .then((res) => {
-  //     response = res;
-  //     return {
-  //       statusCode: 200,
-  //       body: JSON.stringify({
-  //         test: 'Test',
-  //         res,
-  //       }),
-  //     };
-  //   })
-  //   .catch((err) => {
-  //     response = err;
-  //     return {
-  //       statusCode: 200,
-  //       body: JSON.stringify({
-  //         test: 'Test',
-  //         err,
-  //       }),
-  //     };
-  //   });
+  await client
+    .query(
+      q.Let(
+        {
+          user: q.Get(
+            q.Match(q.Index('user_by_email'), body.data.object.customer_email)
+          ),
+          userRef: q.Select('ref', q.Var('user')),
+        },
+        q.Update(q.Var('userRef'), {
+          data: {
+            status: body.status,
+          },
+        })
+      )
+    )
+    .then((res) => {
+      response = res;
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          test: 'Test',
+          res,
+        }),
+      };
+    })
+    .catch((err) => {
+      response = err;
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          test: 'Test',
+          err,
+        }),
+      };
+    });
   return {
     statusCode: 200,
     body: JSON.stringify({
