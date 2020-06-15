59023e838b92663c5a66b30c2d743a5317b028d4
Jarod Peachey <jwpeachey107@aol.com>
2020-06-15 12:04:39 -0400

    Test

--- src/functions/portal.js
+++ src/functions/portal.js
@@ -1,5 +1,6 @@
 /* eslint-disable prefer-arrow-callback */
 const stripeSdk = require('stripe');
+const { callbackPromise } = require('nodemailer/lib/shared');
 const stripe = stripeSdk(
   'pk_test_51Gr3KVKyL3kUtkPFJMQdsezF9hqGudJNNnwfdA9ZdH4i7MCdwni4qjxl32KSe1ClUpdapbLCMUkMeLfBeEHbwm5G00sPUTEKHc'
 );
@@ -8,8 +9,11 @@ exports.handler = async (event, context) => {
   const body = JSON.parse(event.body);
   let response;
 
-  return {
+  callback(null, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     statusCode: 200,
-    body: 'Test',
-  };
+    body: 'test',
+  });
 };
