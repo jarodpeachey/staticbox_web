849b43e08a7b59606f5eff3e4e29cd0f7f73638e
Jarod Peachey <jwpeachey107@aol.com>
2020-06-15 11:02:29 -0400

    Test again

--- src/functions/portal.js
+++ src/functions/portal.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-arrow-callback */
 const stripeSdk = require('stripe');
 const stripe = stripeSdk(
   'pk_test_51Gr3KVKyL3kUtkPFJMQdsezF9hqGudJNNnwfdA9ZdH4i7MCdwni4qjxl32KSe1ClUpdapbLCMUkMeLfBeEHbwm5G00sPUTEKHc'
@@ -5,16 +6,23 @@ const stripe = stripeSdk(
 
 exports.handler = async (event, context) => {
   const body = JSON.parse(event.body);
+  let response;
 
   stripe.billingPortal.sessions.create(
     {
       customer: body.customer,
-      return_url: 'https://app.staticbox.io',
+      return_url: 'https://example.com/account',
     },
     function (err, session) {
-      return {
-        statusCode: 200,
-        body: session,
+      console.log('test');
+      console.log(err);
+
+      response = session;
     }
-  )
+  );
+
+  return {
+    statusCode: 200,
+    body: response,
+  };
 };
