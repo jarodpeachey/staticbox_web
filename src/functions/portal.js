4ccfb1008e5843a160dc395f0601d76de3c1312b
Jarod Peachey <jwpeachey107@aol.com>
2020-06-16 08:15:33 -0400

    More testing functions

--- src/functions/portal.js
+++ src/functions/portal.js
@@ -25,7 +25,7 @@ exports.handler = async function (event, context, callback) {
       callback(null, {
         statusCode: 200,
         body: JSON.stringify({
-          body: { msg: 'Success!', data: res },
+          body: res,
         }),
       });
     })
