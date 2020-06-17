c84d31ac39eb2f03125302830a0cef52c272d226
Jarod Peachey <jwpeachey107@aol.com>
2020-06-17 13:26:03 -0400

    Testing

--- src/functions/portal.js
+++ src/functions/portal.js
@@ -25,7 +25,7 @@ exports.handler = async function (event, context, callback) {
 
       return {
         statusCode: 200,
-        body: session
+        body: JSON.stringify(session),
       };
     }
   );
