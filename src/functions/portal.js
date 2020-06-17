a08032c0392fbdc2411100d0e73f51d930c7187f
Jarod Peachey <jwpeachey107@aol.com>
2020-06-17 13:07:21 -0400

    More updates

--- src/functions/portal.js
+++ src/functions/portal.js
@@ -31,11 +31,4 @@ exports.handler = async function (event, context, callback) {
       };
     }
   );
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      response,
-    }),
-  };
 };
