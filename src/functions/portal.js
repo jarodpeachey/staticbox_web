3554f76e252bbb9a923ce830fab1ce50985a693f
Jarod Peachey <jwpeachey107@aol.com>
2020-06-17 07:45:11 -0400

    More testing

--- src/functions/portal.js
+++ src/functions/portal.js
@@ -22,10 +22,7 @@ exports.handler = async function (event, context, callback) {
 
       callback(null, {
         statusCode: 200,
-        body: JSON.stringify({
-          msg: 'Test',
-          res: "Test",
-        }),
+        body: 'Message',
       });
     }
   );
